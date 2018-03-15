5e77f1f670979e0fda64ffab60b91e1d4250d6af
Bartek Budzyński <kaybeaver@gmail.com>
2018-03-15 01:03:29 +0100

    initial commit

---
+++ src/main/groovy/com/example/myapp/demo/ExampleObject.groovy
@@ -0,0 +1,16 @@
+package com.example.myapp.demo
+
+class ExampleObject{
+    String name
+    String age
+
+    public ExampleObject(name, age){
+        this.age = age
+        this.name = name
+    }
+
+    public ExampleObject(){
+        this.age = 3
+        this.name = "Jas"
+    }
+}
---
+++ src/main/groovy/com/example/myapp/demo/HelloController.groovy
@@ -0,0 +1,23 @@
+package com.example.myapp.demo
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import com.example.myapp.demo.ExampleObject
+
+@RestController
+class HelloController{
+    @RequestMapping("/hello")
+    public sayHello(){
+        return "Hello"
+    }
+
+    @RequestMapping(name =  "/sendnudes", method = RequestMethod.POST)
+    public ResponseEntity<ExampleObject> sendNudes(@RequestBody ExampleObject exampleObject){
+        return new ResponseEntity<ExampleObject>(exampleObject, HttpStatus.OK)
+    }
+
+}
